15684fb8612a6321b3c431e5090fe2057eb2498e
Alberto Baraza <albertoww02@gmail.com>
2020-01-26 21:05:05 +0100

    al

--- proyecto/versionHilos.c
+++ proyecto/versionHilos.c
@@ -10,22 +10,22 @@
 typedef struct{
 	unsigned char **original;
 	int **nucleo;
-	int largo;
+	int ancho;
 	int alto;
 	int k;
-}tipodato;
+}tipoDato;
 
 typedef struct{
-	tipodato *datos;
+	tipoDato *datos;
 	unsigned char **salida;
 	int filainicio;
 	int numfilas;
-}tipohilo;
+}hilo;
 
 void *convolucion(void *parametro){
   int x, y, suma, i, j, f;
 
-  tipohilo *h = (tipohilo *) parametro;
+  hilo *h = (hilo *) parametro;
 
   printf ("\nFila inicio %d  y total %d", h->filainicio, h->numfilas);
   f = h->filainicio;
@@ -51,10 +51,10 @@ void *convolucion(void *parametro){
 
 int main(int argc, char *argv[]){
   int numhilos, i, j, n, fila, numerofilas, excedente;
-  unsigned char** Salida;
-  tipohilo *mishilos;
+  unsigned char** salida;
+  hilo *structHilos;
   pthread_t *mispthread;
-  tipodato misdatos;
+  tipoDato variables;
   bool flag  =true;
 
   while(flag){
@@ -67,66 +67,66 @@ int main(int argc, char *argv[]){
         }
     }
 
-  misdatos.original = pgmread("lena_original.pgm", &misdatos.largo, &misdatos.alto);
-  Salida = (unsigned char**)GetMem2D(misdatos.largo, misdatos.alto, sizeof(unsigned char));
+  variables.original = pgmread("lena_original.pgm", &variables.ancho, &variables.alto);
+  salida = (unsigned char**)GetMem2D(variables.ancho, variables.alto, sizeof(unsigned char));
 
-  misdatos.nucleo = (int**) GetMem2D(tamanoNucleo, tamanoNucleo, sizeof(int));
+  variables.nucleo = (int**) GetMem2D(tamanoNucleo, tamanoNucleo, sizeof(int));
   for (i = 0; i < tamanoNucleo; i++){
     for (j = 0; j < tamanoNucleo; j++){
-      misdatos.nucleo[i][j] = -1;
+      variables.nucleo[i][j] = -1;
     }
-  }misdatos.nucleo[tamanoNucleo/2][tamanoNucleo/2] = 1;
+  }variables.nucleo[tamanoNucleo/2][tamanoNucleo/2] = 1;
   
-  misdatos.k = 0;
+  variables.k = 0;
   
   for (i = 0; i < tamanoNucleo; i++){
     for (j = 0; j < tamanoNucleo; j++){
-      misdatos.k = misdatos.k + misdatos.nucleo[i][j];
+      variables.k = variables.k + variables.nucleo[i][j];
     }
   }
   
-  mishilos = (tipohilo *) malloc (sizeof(tipohilo) * numhilos);
+  structHilos = (hilo *) malloc (sizeof(hilo) * numhilos);
   mispthread = (pthread_t *) malloc (sizeof(pthread_t) * numhilos);
 
   fila = 1;
-  numerofilas = (misdatos.largo-2) / numhilos;
-  excedente = (misdatos.largo-2) % numhilos;
+  numerofilas = (variables.ancho-2) / numhilos;
+  excedente = (variables.ancho-2) % numhilos;
   for (i=0; i<numhilos; i++){
-    mishilos[i].datos = &misdatos;
-    mishilos[i].filainicio = fila;
-    mishilos[i].numfilas = numerofilas;
+    structHilos[i].datos = &variables;
+    structHilos[i].filainicio = fila;
+    structHilos[i].numfilas = numerofilas;
 
       if (excedente > 0){
-        mishilos[i].numfilas++;
+        structHilos[i].numfilas++;
         excedente--;
       }
 
-    mishilos[i].salida = (unsigned char**)GetMem2D(mishilos[i].numfilas, misdatos.alto, sizeof(unsigned char));
-    pthread_create (&mispthread[i], NULL, convolucion, &mishilos[i]);
-    fila += mishilos[i].numfilas;
+    structHilos[i].salida = (unsigned char**)GetMem2D(structHilos[i].numfilas, variables.alto, sizeof(unsigned char));
+    pthread_create (&mispthread[i], NULL, convolucion, &structHilos[i]);
+    fila += structHilos[i].numfilas;
     }
 
   for (n=0; n<numhilos; n++){
-	  pthread_join (mispthread[n], 0);
-	  for (i=0; i<mishilos[n].numfilas; i++)
+	  pthread_join (mispthread[n], NULL);
+	  for (i=0; i<structHilos[n].numfilas; i++)
     {
-      for (j=1; j<misdatos.alto-1; j++)
+      for (j=1; j<variables.alto-1; j++)
       {
-        Salida[mishilos[n].filainicio + i][j] = mishilos[n].salida[i][j];
+        salida[structHilos[n].filainicio + i][j] = structHilos[n].salida[i][j];
       }
     }
   }
 
-  pgmwrite(Salida, "lena_procesada_hilos.pgm", misdatos.largo, misdatos.alto);
+  pgmwrite(salida, "lena_procesada_hilos.pgm", variables.ancho, variables.alto);
 
-  Free2D((void**) misdatos.nucleo, tamanoNucleo);
+  Free2D((void**) variables.nucleo, tamanoNucleo);
 
-  Free2D((void**) misdatos.original, misdatos.largo);
-  Free2D((void**) Salida, misdatos.largo);
+  Free2D((void**) variables.original, variables.ancho);
+  Free2D((void**) salida, variables.ancho);
   for (i=0; i<numhilos; i++)
   {
-     Free2D ((void**) mishilos[i].salida, mishilos[i].numfilas);
+     Free2D ((void**) structHilos[i].salida, structHilos[i].numfilas);
   }
-  free (mishilos);
+  free (structHilos);
   free (mispthread);
 }
